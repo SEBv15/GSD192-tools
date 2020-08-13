b1e85d3591706793eccc32b1a5b2e09ae66bb849
Sebastian <sebastian@strempfer.com>
2020-08-13 17:27:16 -0500

    Update __init__.py

--- gsd192_tools/__init__.py
+++ gsd192_tools/__init__.py
@@ -1,4 +1,5 @@
 from .configure import configure
 from .zclient import zclient
-from . import calibration
+from . import calibration # Vastly improved version. Reduced amount extra of code needed to use library drastically and curve fitting is significantly faster.
+from . import calibration_v1 # For legacy code
 from . import bluesky
